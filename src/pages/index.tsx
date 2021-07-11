49767fee7c94b752296864c95518f6d193ee338d
GameDog9988 <alex@grattan.me>
2021-07-11 19:43:48 -0400

    feat: host images on imgbb

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,30 @@
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 import tw from "twin.macro";
-("twin.macro");
-import { motion } from "framer-motion";
-import CSWiki from "../images/cs_wiki.png";
-import Dev from "../images/dev_community.png";
-import CSSTricks from "../images/css_tricks.png";
 
 const Home = (): JSX.Element => {
 	return (
 		<Layout>
 			<SEO />
 			<main tw="text-white text-center bg-gradient-to-r px-4 from-primary to-blue-700 flex justify-center items-center flex-col space-y-8 w-full h-screen">
-				<motion.h1
-					initial={{ y: 50, opacity: 0 }}
-					animate={{
-						y: 0,
-						opacity: 1,
-						transition: {
-							duration: 0.8,
-							delay: 0.2,
-							type: "tween",
-							ease: "circOut",
-						},
-					}}
+				<h1
+					css={[
+						tw`font-extralight text-3xl md:text-5xl lg:text-7xl`,
+					]}
 				>
 					Pitt CSC Web-Dev Branch{" "}
 					<span tw="inline-block transform transition hover:scale-110">
 						👩‍💻
 					</span>
-				</motion.h1>
-				<motion.div
-					initial={{ y: 50, opacity: 0 }}
-					animate={{
-						y: 0,
-						opacity: 1,
-						transition: {
-							duration: 0.8,
-							delay: 0.4,
-							type: "tween",
-							ease: "circOut",
-						},
-					}}
+				</h1>
+				<div
+					css={[
+						tw`bg-secondary-200 w-full max-w-6xl p-8 rounded-2xl text-black`,
+					]}
 				>
 					<div tw="flex flex-col space-y-8 justify-center items-center md:space-y-0 md:flex-row md:justify-between">
 						<a
-							href="https://dev.to/"
+							href="https://pittcs.wiki/skills/getting-started-with-x/web-dev/"
 							rel="noreferrer noopener"
 							target="_blank"
 						>
@@ -54,7 +33,7 @@ const Home = (): JSX.Element => {
 								tw="w-full md:w-80 text-center space-y-2"
 							>
 								<img
-									src={CSWiki}
+									src="https://i.ibb.co/LgnJ94t/cs-wiki.png"
 									alt="Pitt CS Wiki Web Dev Getting Started"
 									tw="z-10 relative rounded shadow-md"
 								/>
@@ -73,7 +52,7 @@ const Home = (): JSX.Element => {
 								tw="w-full md:w-80 text-center space-y-2"
 							>
 								<img
-									src={Dev}
+									src="https://i.ibb.co/WVJJNWF/dev-community.png"
 									alt="Dev Community"
 									tw="z-10 relative rounded shadow-md"
 								/>
@@ -92,7 +71,7 @@ const Home = (): JSX.Element => {
 								tw="w-full md:w-80 text-center space-y-2"
 							>
 								<img
-									src={CSSTricks}
+									src="https://i.ibb.co/fdF6Q0B/css-tricks.png"
 									alt="CSS Tricks"
 									tw="z-10 relative rounded shadow-md"
 								/>
@@ -102,7 +81,7 @@ const Home = (): JSX.Element => {
 							</div>
 						</a>
 					</div>
-				</motion.div>
+				</div>
 			</main>
 		</Layout>
 	);
