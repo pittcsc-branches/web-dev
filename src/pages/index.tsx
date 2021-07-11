3821004147e8710479c0b4c6e98e535f28c22771
GameDog9988 <alex@grattan.me>
2021-07-11 17:35:46 -0400

    feat: rename images and switch around framer motion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,54 @@
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
-import "twin.macro";
-import CSWiki from "../images/cs-wiki.png";
-import Dev from "../images/dev-community.png";
-import CSSTricks from "../images/css-tricks.png";
+import tw from "twin.macro";
+("twin.macro");
+import { motion } from "framer-motion";
+import CSWiki from "../images/cs_wiki.png";
+import Dev from "../images/dev_community.png";
+import CSSTricks from "../images/css_tricks.png";
 
 const Home = (): JSX.Element => {
 	return (
 		<Layout>
 			<SEO />
 			<main tw="text-white text-center bg-gradient-to-r px-4 from-primary to-blue-700 flex justify-center items-center flex-col space-y-8 w-full h-screen">
-				<h1 tw="font-extralight text-3xl md:text-5xl lg:text-7xl">
+				<motion.h1
+					initial={{ y: 50, opacity: 0 }}
+					animate={{
+						y: 0,
+						opacity: 1,
+						transition: {
+							duration: 0.8,
+							delay: 0.2,
+							type: "tween",
+							ease: "circOut",
+						},
+					}}
+					css={[
+						tw`font-extralight text-3xl md:text-5xl lg:text-7xl`,
+					]}
+				>
 					Pitt CSC Web-Dev Branch{" "}
 					<span tw="inline-block transform transition hover:scale-110">
 						👩‍💻
 					</span>
-				</h1>
-				<div tw="bg-secondary-200 w-full max-w-6xl p-8 rounded-2xl text-black">
+				</motion.h1>
+				<motion.div
+					initial={{ y: 50, opacity: 0 }}
+					animate={{
+						y: 0,
+						opacity: 1,
+						transition: {
+							duration: 0.8,
+							delay: 0.4,
+							type: "tween",
+							ease: "circOut",
+						},
+					}}
+					css={[
+						tw`bg-secondary-200 w-full max-w-6xl p-8 rounded-2xl text-black`,
+					]}
+				>
 					<div tw="flex flex-col space-y-8 justify-center items-center md:space-y-0 md:flex-row md:justify-between">
 						<a
 							href="https://dev.to/"
@@ -76,7 +108,7 @@ const Home = (): JSX.Element => {
 							</div>
 						</a>
 					</div>
-				</div>
+				</motion.div>
 			</main>
 		</Layout>
 	);
